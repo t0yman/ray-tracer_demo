d2a4d173ab10ace2bc811c621360c0d99b2ed419
t0yman <195047463+t0yman@users.noreply.github.com>
2025-10-20 10:09:15 -0700

    Gradient PPM image demo (Math utils integration)

--- source/main.cpp
+++ source/main.cpp
@@ -2,11 +2,30 @@
 #include <fstream>
 #include <stdexcept>
 
+#include "color.hpp"
+#include "ray.hpp"
+
+Color GetRayColor(const Ray& ray);
+
 int main()
 {
-    const int imageWidth = 256;
-    const int imageHeight = 256;
+    // image config
+    const double aspectRatio = 16.0 / 9.0;
+ 
+    const int imageWidth = 400;
+    const int imageHeight = static_cast<int>(imageWidth / aspectRatio);
 
+    // camera config
+    const double viewportHeight = 2.0;
+    const double viewportWidth = aspectRatio * viewportHeight;
+    const double focalLength = 1.0;
+
+    const Point3 cameraOrigin = Point3{0.0, 0.0, 0.0};
+    const Vector3 horizontalDir = Vector3{viewportWidth, 0.0, 0.0};  // represents the horizontal direction in which the viewport spans
+    const Vector3 verticalDir = Vector3{0.0, viewportHeight, 0.0};  // represents the vertical direction in which the viewport spans
+    const Point3 lowerLeftCorner = cameraOrigin - (horizontalDir / 2.0) - (verticalDir / 2.0) - Vector3{0.0, 0.0, focalLength};
+
+    // rendering 
     std::ofstream imageFile{"output.ppm"};
     if (imageFile.is_open() == false)
     {
@@ -14,23 +33,39 @@ int main()
     }
 
     imageFile << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
-    for (int y = 0; y < imageHeight; ++y)
+
+    for (int y = imageHeight - 1; y >= 0; --y)
     {
         for (int x = 0; x < imageWidth; ++x)
         {
-            // calculate normalized coordinates [0.0, 1.0]
             const double u = static_cast<double>(x) / (imageWidth - 1);
             const double v = static_cast<double>(y) / (imageHeight - 1);
 
-            // blend from white (top) to blue (bottom)
-            const int r = static_cast<int>(255.999 * (1.0 - v + v * 0.5));  // white to light blue
-            const int g = static_cast<int>(255.999 * (1.0 - v + v * 0.7));
-            const int b = static_cast<int>(255.999 * 1.0);
+            // lowerLeftCorner + horizontalDir * u + verticalDir * v
+            // calculates a point on the viewport for pixel(u, v) = vewportPoint
+            // viewportPoint - cameraOrigin
+            // (destination point) - (starting point) gives the direction from the camera to the point
+            Ray ray{cameraOrigin, lowerLeftCorner + horizontalDir * u + verticalDir * v - cameraOrigin};
+            
+            Color pixelColor = GetRayColor(ray);
+
+            const int r = static_cast<int>(255.999 * pixelColor.x);
+            const int g = static_cast<int>(255.999 * pixelColor.y);
+            const int b = static_cast<int>(255.999 * pixelColor.z);
 
             imageFile << r << " " << g << " " << b << "\n";
         }
     }
+
     imageFile.close();
 
     return 0;
+}
+
+Color GetRayColor(const Ray& ray)
+{
+    const Vector3 direction = Normalize(ray.direction);
+    const double t = 0.5 * (direction.y + 1.0);
+
+    return Color{1.0, 1.0, 1.0} * (1.0 - t) + Color{0.5, 0.7, 1.0} * t;
 }
